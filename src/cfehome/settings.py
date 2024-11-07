30a84a908cf857294bba96abef799b2d21c36de8
CodingForEntrepreneurs <hello@teamcfe.com>
2024-11-07 13:38:30 -0700

    14 - Django & Timescale with Docker Compose

--- src/cfehome/settings.py
+++ src/cfehome/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -80,6 +81,17 @@ DATABASES = {
     }
 }
 
+DATABASE_URL = config("DATABASE_URL", default="", cast=str)
+
+if DATABASE_URL != "":
+    import dj_database_url
+    DATABASES = {
+        "default": dj_database_url.config(
+            default=DATABASE_URL,
+            conn_max_age=300,
+            engine='timescale.db.backends.postgresql',
+        )
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
